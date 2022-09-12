b7f91285d1473e933d7190279b38ebfb3e3f6130
kobeomseok95 <kobumssh@naver.com>
2022-09-12 18:54:42 +0900

    typescript 설정

--- react-netflix-clone/src/pages/DetailPage/index.js
+++ react-netflix-clone/src/pages/DetailPage/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import MovieApiClient from '../../api/MovieApiClient';
 
-function DetailPage() {
+const DetailPage = () => {
   const { movieId } = useParams();
   const [ movie, setMovie ] = useState({});
   
